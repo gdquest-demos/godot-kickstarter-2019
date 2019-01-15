b08fa40cb3f7ad313801d166c511fb12f0176acf
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-01-15 19:46:03 -0200

    Random item specs demo added

---
+++ 01-14-random-item-specs/items/specs/Spec.gd
@@ -0,0 +1,8 @@
+extends Node
+class_name Spec
+
+var value : = 0
+
+func initialize(_name : String, _value : int) -> void:
+	name = _name
+	value = _value
---
+++ 01-14-random-item-specs/items/specs/SpecResource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name SpecResource
+
+export var mandatory : bool = false
+export(float, 0.0, 1.0) var chance : float = 1.0
+export var spec_name : String
+export var min_value : int
+export var max_value : int
