a24de75b6051dd825870f08de1bb9f736ae425b3
Nathan Lovato <nathan@gdquest.com>
2019-01-24 13:22:03 +0900

    Tweak code in FollowerCamera demo
    Using type inference for Rect2, avoiding long lines for max and Vector2 calls

--- 01-23-camera-follow-2d/camera/FollowerCamera.gd
+++ 01-23-camera-follow-2d/camera/FollowerCamera.gd
@@ -3,15 +3,13 @@ extends Camera2D
 export(float, 0.1, 0.5) var zoom_offset : float = 0.2
 export var debug_mode : bool = false
 
-var camera_rect : Rect2
-var viewport_rect : Rect2
+var camera_rect : = Rect2()
+var viewport_rect : = Rect2()
 
 
 func _ready() -> void:
 	viewport_rect = get_viewport_rect()
 	set_process(get_child_count() > 0)
-	for child in get_children():
-		child.set_as_toplevel(true)
 
 
 func _process(delta: float) -> void:
@@ -28,17 +26,21 @@ func _process(delta: float) -> void:
 		update()
 
 
+func calculate_center(rect: Rect2) -> Vector2:
+	return Vector2(
+		rect.position.x + rect.size.x / 2,
+		rect.position.y + rect.size.y / 2)
+
+
+func calculate_zoom(rect: Rect2, viewport_size: Vector2) -> Vector2:
+	var max_zoom = max(
+		max(1, rect.size.x / viewport_size.x + zoom_offset),
+		max(1, rect.size.y / viewport_size.y  + zoom_offset))
+	return Vector2(max_zoom, max_zoom)
+
+
 func _draw() -> void:
 	if not debug_mode:
 		return
 	draw_rect(camera_rect, Color("#ffffff"), false)
 	draw_circle(calculate_center(camera_rect), 5, Color("#ffffff"))
-
-
-func calculate_center(rect: Rect2) -> Vector2:
-	return Vector2(rect.position.x + rect.size.x / 2, rect.position.y + rect.size.y / 2)
-
-
-func calculate_zoom(rect: Rect2, viewport_size: Vector2) -> Vector2:
-	var max_zoom = max(max(1, rect.size.x / viewport_size.x + zoom_offset), max(1, rect.size.y / viewport_size.y  + zoom_offset))
-	return Vector2(max_zoom, max_zoom)
