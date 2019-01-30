a4bfcb5c586b01631d16c335984dcaf41ece1fac
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-01-30 14:03:32 -0200

    Converted boxes to kinematicbodies

--- 01-29-puzzle-game/player/Player.gd
+++ 01-29-puzzle-game/player/Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 onready var animated_sprite : AnimatedSprite = $AnimatedSprite
 
-export var move_speed : = 50.0
+export var move_speed : = 250.0
+export var push_speed : = 125.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +12,9 @@ func _physics_process(delta: float) -> void:
 	motion.y = int(Input.get_action_strength("move_down")) - int(Input.get_action_strength("move_up"))
 	
 	update_animation(motion)
-	move_and_collide(motion.normalized() * move_speed * delta)
+	move_and_slide(motion.normalized() * move_speed, Vector2())
+	if get_slide_count() > 0:
+		check_box_collision(motion)
 
 
 func update_animation(motion: Vector2) -> void:
@@ -28,3 +31,11 @@ func update_animation(motion: Vector2) -> void:
 	
 	if animated_sprite.animation != animation:
 		animated_sprite.play(animation)
+
+
+func check_box_collision(motion: Vector2) -> void:
+	if abs(motion.x) + abs(motion.y) > 1:
+		return
+	var box : = get_slide_collision(0).collider as Box
+	if box:
+		box.push(push_speed * motion)
--- 01-29-puzzle-game/world/props/Box.gd
+++ 01-29-puzzle-game/world/props/Box.gd
@@ -1,2 +1,6 @@
-extends RigidBody2D
+extends KinematicBody2D
 class_name Box
+
+
+func push(velocity: Vector2) -> void:
+	move_and_slide(velocity, Vector2())
