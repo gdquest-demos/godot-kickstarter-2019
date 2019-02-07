1a2c86114e64621578b08ebbc99a6d9861798077
Nathan Lovato <nathan@gdquest.com>
2019-02-08 08:50:41 +0900

    Update components in theme: ColorPalette and rename TextEdit

---
+++ gdquest-godot-theme/src/Components/ColorPalette/ColorSwatch/ColorSwatch.gd
@@ -0,0 +1,18 @@
+tool
+extends Button
+
+
+onready var color_rect : ColorRect = $ColorRect
+
+export var color : = Color('ffffff') setget set_color
+
+
+func set_color(value:Color) -> void:
+	color = value
+	if not color_rect:
+		return
+	color_rect.color = value
+
+
+func _ready() -> void:
+	self.color = color
