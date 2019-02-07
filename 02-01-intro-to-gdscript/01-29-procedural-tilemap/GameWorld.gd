88dc10b6808334d5b13bc06ed99354654e7cf518
Nathan Lovato <nathan@gdquest.com>
2019-02-08 08:49:53 +0900

    Tweak names in PCG GDScript intro demo

--- 02-01-intro-to-gdscript/01-29-procedural-tilemap/GameWorld.gd
+++ 02-01-intro-to-gdscript/01-29-procedural-tilemap/GameWorld.gd
@@ -32,9 +32,11 @@ func setup() -> void:
 
 
 func generate() -> void:
-	# Although there's no other nodes to use these signals, we're including them
-	# to show when and how to emit them.
-	# Watch our signals tutorial for more information.
+	"""
+	Although there's no other nodes to use these signals, we're including them
+	to show when and how to emit them.
+	Watch our signals tutorial for more information.
+	"""
 	emit_signal("started")
 	generate_perimeter()
 	generate_inner()
@@ -44,51 +46,51 @@ func generate() -> void:
 func generate_perimeter() -> void:
 	"""
 	Fills the outer edges of the map with the border tiles.
-	Randomly selects from the tiles marked as `Cell.OUTER` using the funciton `pick_cell`.
+	Randomly selects from the tiles marked as `Cell.OUTER` using the funciton `_pick_random_texture`.
 	"""
 	# The two nested loops below fill the outer columns and outer row
 	# of the map, respectively.
 	for x in [0, size.x - 1]:
 		for y in range(0, size.y):
-			_tile_map.set_cell(x, y, pick_cell(Cell.OUTER))
+			_tile_map.set_cell(x, y, _pick_random_texture(Cell.OUTER))
 	for x in range(1, size.x - 1):
 		for y in [0, size.y - 1]:
-			_tile_map.set_cell(x, y, pick_cell(Cell.OUTER))
+			_tile_map.set_cell(x, y, _pick_random_texture(Cell.OUTER))
 
 
 func generate_inner() -> void:
 	"""
 	Fills the inside of the map the inner tiles from the remaining types: `Cell.GROUND` and `Cell.OBSTACLE` using the
-	`rand_cell` function that takes the probability for `Cell.GROUND` tiles to have some more control
+	`get_random_tile` function that takes the probability for `Cell.GROUND` tiles to have some more control
 	over what types of tiles we'll be placing.
 	"""
 	for x in range(1, size.x - 1):
 		for y in range(1, size.y - 1):
-			var cell : = rand_cell(ground_probability)
+			var cell : = get_random_tile(ground_probability)
 			_tile_map.set_cell(x, y, cell)
 
 
-func pick_cell(type: int) -> int:
+func get_random_tile(probability: float) -> int:
+	"""
+	Randomly picks a tile id between `Cell.GROUND` and `Cell.OBSTACLE` types given the ground probability.
+	Returns the id of the cell in the TileSet resource.
+	"""
+	return _pick_random_texture(Cell.GROUND) if _rng.randf() < probability else _pick_random_texture(Cell.OBSTACLE)
+
+
+func _pick_random_texture(cell_type: int) -> int:
 	"""
 	Randomly picks a tile based on the three types: `Cell.OUTER`, `Cell.GROUND` & `Cell.OBSTACLE`.
 	Returns the id of the cell in the TileSet resource.
 	"""
 	var interval : = Vector2()
-	if type == Cell.OUTER:
+	if cell_type == Cell.OUTER:
 		interval = Vector2(0, 9)
-	elif type == Cell.GROUND:
+	elif cell_type == Cell.GROUND:
 		interval = Vector2(10, 14)
-	elif type == Cell.OBSTACLE:
+	elif cell_type == Cell.OBSTACLE:
 		interval = Vector2(15, 27)
 	return _rng.randi_range(interval.x, interval.y)
-
-
-func rand_cell(probability: float) -> int:
-	"""
-	Randomly picks a tile id between `Cell.GROUND` and `Cell.OBSTACLE` types given the ground probability.
-	Returns the id of the cell in the TileSet resource.
-	"""
-	return pick_cell(Cell.GROUND) if _rng.randf() < probability else pick_cell(Cell.OBSTACLE)
 
 
 func _ready() -> void:
