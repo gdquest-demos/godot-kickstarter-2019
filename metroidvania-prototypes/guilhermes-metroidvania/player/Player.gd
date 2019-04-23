fa0d9d3299e925596afecbd3dea579e29e6930be
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-23 10:44:54 -0300

    Player movement added

---
+++ metroidvania-prototypes/guilhermes-metroidvania/player/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export var move_speed := 750
+export var jump_force := 1200
+export var gravity := 2000
+export var acceleration := 2.5
+export var deceleration := 7.5
+
+var velocity := Vector2()
+
+
+func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var desired_x_velocity = direction_x * move_speed
+	var weight = deceleration * delta if desired_x_velocity == 0 else acceleration * delta
+	velocity.x = lerp(velocity.x, desired_x_velocity, weight)
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+	
+	velocity.y += gravity * delta
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
