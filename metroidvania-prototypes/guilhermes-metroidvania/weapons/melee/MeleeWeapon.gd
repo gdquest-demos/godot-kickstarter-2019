9de2cc1de74b69670d6180c9fad7417a948ef057
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-23 11:15:19 -0300

    Melee weapon added

---
+++ metroidvania-prototypes/guilhermes-metroidvania/weapons/melee/MeleeWeapon.gd
@@ -0,0 +1,23 @@
+extends Weapon
+
+onready var hit_box : Area2D = $HitBox
+
+export var attack_range := 196
+
+
+func _ready() -> void:
+	hit_box.connect("body_entered", self, "_on_HitBox_body_entered")
+
+
+func _on_HitBox_body_entered(body: PhysicsBody2D) -> void:
+	if body.is_a_parent_of(self):
+		return
+	#TODO: Damage enemies
+
+
+func use_weapon() -> void:
+	hit_box.position = Vector2(attack_range * attack_direction, 0)
+	hit_box.active = true
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	hit_box.active = false
