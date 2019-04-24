24fc1b8d01ed21d14186edbc5a92a333d53dbcc7
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-24 15:56:14 -0300

    Added door opening effect and sound

--- metroidvania-prototypes/guilhermes-metroidvania/world/DoorTrigger.gd
+++ metroidvania-prototypes/guilhermes-metroidvania/world/DoorTrigger.gd
@@ -1,8 +1,12 @@
 extends Interactive
 
+export var door : NodePath
+
+onready var collision_shape : CollisionShape2D = $CollisionShape2D
+onready var sprite : Sprite = $Sprite
+
 
 func interact() -> void:
-	for child in get_children():
-		if child is Door:
-			(child as Door).open()
+	assert get_node(door) is Door
+	(get_node(door) as Door).open()
 	queue_free()
