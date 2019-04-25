6d50ccf68d075eb6799eae09b34ba1eda6880dcc
Nathan Lovato <nathan@gdquest.com>
2019-04-25 17:01:48 +0900

    Add hook-based movement prototype

--- metroidvania-prototypes/hook!/Arrow.gd
+++ metroidvania-prototypes/hook!/Arrow.gd
@@ -2,10 +2,30 @@ extends Node2D
 
 onready var head: = $Head
 onready var tail: = $Tail
+onready var tween: Tween = $Tween
 
-var target_length: = 40.0 setget set_target_length
+var hook_position: = Vector2.ZERO setget set_hook_position
+var length: = 40.0 setget set_length
 
-func set_target_length(value: float) -> void:
-	target_length = value
-	tail.points[-1].x = target_length
-	head.position.x = tail.points[-1].x
+onready var start_length: float = head.position.x
+
+func set_hook_position(value:Vector2) -> void:
+	hook_position = value
+	var to_target: = hook_position - global_position
+	self.length = to_target.length()
+	rotation = to_target.angle()
+	tween.interpolate_property(
+		self, 'length', length, start_length, 
+		0.25, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.start()
+	visible = true
+
+
+func set_length(value: float) -> void:
+	length = value
+	tail.points[-1].x = length
+	head.position.x = tail.points[-1].x + tail.position.x
+
+
+func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
+	visible = false
--- metroidvania-prototypes/hook!/HookingPoint.gd
+++ metroidvania-prototypes/hook!/HookingPoint.gd
@@ -1,27 +1,22 @@
 tool
-extends Position2D
+extends DrawingUtils
+"""Draws a target to indicate if and where the player can hook"""
 
-const COLOR_BLUE_LIGHT: = Color("09a6ca")
-const COLOR_BLUE_DEEP: = Color("0046ff")
+export var color_hook: Color = COLOR_BLUE_LIGHT
+export var color_cooldown: Color = COLOR_ERROR
 
-const COLOR_SUCCESS: = Color("2cb638")
-const COLOR_WARNING: = Color("ff9b00")
-const COLOR_ERROR: = Color("ff004e")
-
-const DEFAULT_POINTS_COUNT: = 32
+var color: = color_hook setget set_color
 
 func _ready() -> void:
 	set_as_toplevel(true)
 	update()
 
-func _draw() -> void:
-	draw_circle_outline(Vector2.ZERO, 20.0, COLOR_BLUE_LIGHT, 4.0)
-	draw_circle(Vector2.ZERO, 10.0, COLOR_BLUE_LIGHT)
 
-func draw_circle_outline(position:=Vector2.ZERO, radius:float=30.0, color:=Color(), thickness:=1.0) -> void:
-	var points_array: = PoolVector2Array()
-	for i in range(DEFAULT_POINTS_COUNT + 1):
-		var angle: = 2 * PI * i / DEFAULT_POINTS_COUNT
-		var point: = position + Vector2(cos(angle) * radius, sin(angle) * radius)
-		points_array.append(point)
-	draw_polyline(points_array, color, thickness, true)
+func _draw() -> void:
+	draw_circle_outline(self, Vector2.ZERO, 20.0, color, 4.0)
+	draw_circle(Vector2.ZERO, 10.0, color)
+
+
+func set_color(value:Color) -> void:
+	color = value
+	update()
